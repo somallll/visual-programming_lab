3234e954fedd99ef9fd4efe3356dc18187a3f5dd
somallll <233530@students.au.edu.pk>
2024-11-13 01:22:40 +0500

    Add files via upload

---
+++ Lab6/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Lab_06
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnSort = new System.Windows.Forms.Button();
+            this.btnDisplay = new System.Windows.Forms.Button();
+            this.lstBoxData = new System.Windows.Forms.ListBox();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(493, 68);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(493, 118);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(75, 23);
+            this.btnRemove.TabIndex = 1;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnSort
+            // 
+            this.btnSort.Location = new System.Drawing.Point(493, 163);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(75, 23);
+            this.btnSort.TabIndex = 2;
+            this.btnSort.Text = "Sort";
+            this.btnSort.UseVisualStyleBackColor = true;
+            this.btnSort.Click += new System.EventHandler(this.btnSort_Click);
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.Location = new System.Drawing.Point(493, 212);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(75, 23);
+            this.btnDisplay.TabIndex = 3;
+            this.btnDisplay.Text = "Display";
+            this.btnDisplay.UseVisualStyleBackColor = true;
+            this.btnDisplay.Click += new System.EventHandler(this.btnDisplay_Click);
+            // 
+            // lstBoxData
+            // 
+            this.lstBoxData.FormattingEnabled = true;
+            this.lstBoxData.ItemHeight = 20;
+            this.lstBoxData.Location = new System.Drawing.Point(219, 89);
+            this.lstBoxData.Name = "lstBoxData";
+            this.lstBoxData.Size = new System.Drawing.Size(120, 84);
+            this.lstBoxData.TabIndex = 4;
+            this.lstBoxData.SelectedIndexChanged += new System.EventHandler(this.lstBoxData_SelectedIndexChanged);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(219, 257);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 26);
+            this.txtInput.TabIndex = 5;
+            this.txtInput.TextChanged += new System.EventHandler(this.txtInput_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(717, 450);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lstBoxData);
+            this.Controls.Add(this.btnDisplay);
+            this.Controls.Add(this.btnSort);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnSort;
+        private System.Windows.Forms.Button btnDisplay;
+        private System.Windows.Forms.ListBox lstBoxData;
+        private System.Windows.Forms.TextBox txtInput;
+    }
+}
+
---
+++ Lab6/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_06
+{
+    public partial class Form1 : Form
+    {
+        private List<string> dataList;
+        public Form1()
+        {
+            InitializeComponent();
+            dataList = new List<string>();
+
+        }
+
+        private void lstBoxData_SelectedIndexChanged(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string item = txtInput.Text.Trim(); // Get input from TextBox
+            if (!string.IsNullOrEmpty(item))
+            {
+                dataList.Add(item); // Add item to List
+                txtInput.Clear();    // Clear the input TextBox
+                DisplayList();       // Refresh the ListBox display
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid item.");
+            }
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            string item = txtInput.Text.Trim(); // Get input from TextBox
+            if (dataList.Contains(item))
+            {
+                dataList.Remove(item); // Remove item from List
+                txtInput.Clear();      // Clear the input TextBox
+                DisplayList();         // Refresh the ListBox display
+            }
+            else
+            {
+                MessageBox.Show("Item not found in the list.");
+            }
+        }
+
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            dataList.Sort(); // Sort the List alphabetically
+            DisplayList();   // Refresh the ListBox display
+        }
+
+        private void btnDisplay_Click(object sender, EventArgs e)
+        {
+            DisplayList();
+        }
+
+        private void txtInput_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void DisplayList()
+        {
+            lstBoxData.Items.Clear(); // Clear the ListBox
+            foreach (var item in dataList)
+            {
+                lstBoxData.Items.Add(item); // Add items to ListBox
+            }
+
+        }
+    }
+}
